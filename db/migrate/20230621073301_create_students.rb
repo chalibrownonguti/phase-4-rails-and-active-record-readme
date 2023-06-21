d057b12b43bf0fe91c4223651a9b52d362b4365e
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-06-21 12:05:42 +0300

    update

--- db/migrate/20230621073301_create_students.rb
+++ db/migrate/20230621073301_create_students.rb
@@ -1,8 +1,8 @@
 class CreateStudents < ActiveRecord::Migration[6.1]
   def change
     create_table :students do |t|
-      t.string :first_name
-      t.string :last_name
+      t.string :first_name, null: false
+      t.string :last_name, null: false
       t.integer :grade
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 2023_06_21_073301) do
 
   create_table "students", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.integer "grade"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
